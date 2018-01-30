0e0bff0794f892cc190635d0c9430b81e7d48939
Ali Ünal <0033484@gmail.com>
2018-01-30 05:57:08 +0300

    Basic Jumping Added

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float jumpSpeed = 5f;
+    Rigidbody rb;
+
+	void Start () {
+
+        rb = GetComponent<Rigidbody>();
+
+	}
+	
+	void Update () {
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            rb.velocity = Vector3.up * jumpSpeed;
+        }
+		
+	}
+}
